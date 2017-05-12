6851771b45cbcba8a92be86724323c4321096826
Irakli Jishkariani <irakli.jishkariani@hizlitahsilat.com>
2017-05-12 19:39:17 +0400

    correct yaml pg config tabulation

--- generators/pg_main/dbConfig.js
+++ generators/pg_main/dbConfig.js
@@ -6,19 +6,20 @@ exports.writeDbParamsToConfig = function (generator) {
 
   let contents = generator.fs.read(configPath);
 
-  contents += '\n' + getParamsString(dbName);
+  contents += getParamsString(dbName);
 
   generator.fs.write(configPath, contents);
 };
 
 function getParamsString(dbName) {
   let dbParams =
-    `db:
-      host: 'localhost'
-      port: '5432'
-      database: '${dbName}'
-      user: 'postgres'
-      password: '123456789'`;
+    `
+db:
+  host: 'localhost'
+  port: '5432'
+  database: '${dbName}'
+  user: 'postgres'
+  password: '123456789'`;
 
   return dbParams;
 }
