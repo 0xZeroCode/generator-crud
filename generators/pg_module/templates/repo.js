40b4938b862e7377cfffff904564d99152186908
Irakli <irakli2692@gmail.com>
2017-07-03 12:18:12 +0400

    repo template changes, fix error on empty array in ejs

--- generators/pg_module/templates/repo.js
+++ generators/pg_module/templates/repo.js
@@ -4,15 +4,14 @@ const tableName = '<%= table %>';
 
 const tableFields = [
   'id',
-  <% for (let i = 0; i < fields.length - 1; i++) { %>'<%= fields[i].name %>',
-  <% } %>'<%= fields[fields.length - 1].name %>',
-  'create_date',
+  <% for (let i = 0; i < fields.length; i++) { %>'<%= fields[i].name %>',
+  <% } %>'create_date',
   'update_date'
 ];
 
 const searchableFields = [
   <% for (let i = 0; i < searchableFields.length - 1; i++) { %>'<%= searchableFields[i].name %>',
-  <% } %>'<%= searchableFields[searchableFields.length - 1].name %>'
+  <% } %><% if (searchableFields.length > 0) {%>'<%= searchableFields[searchableFields.length - 1].name %>'<% } %>
 ];
 
 const repo = new Repository(tableName, tableFields, searchableFields);
