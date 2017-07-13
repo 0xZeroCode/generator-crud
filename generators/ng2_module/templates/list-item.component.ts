09aaf174524b75420eedaae7a10c2e83dec2698a
irakli jiishkariani <irakli2692@gmail.com>
2017-07-13 18:47:49 +0400

    list item component templates. without date pipe

---
+++ generators/ng2_module/templates/list-item.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: '<%= listItemComponentTagName %>',
+    templateUrl: '<%= listItemComponentFileName %>.component.html',
+})
+export class <%= listItemComponentName %> implements OnInit {
+    @Input() <%= model %>: any;
+    @Output() modalToggle = new EventEmitter();
+
+    <%= model %>ToUpdate: any = {};
+
+    constructor() { }
+
+    ngOnInit() {
+        Object.assign(this.<%= model %>ToUpdate, this.<%= model %>);
+    }
+
+    onUpdate(result) {
+        Object.assign(this.<%= model %>, this.<%= model %>ToUpdate);
+    }
+
+    cancel() {
+        Object.assign(this.<%= model %>ToUpdate, this.<%= model %>);
+    }
+
+    toggleModal() {
+        const info = {
+            <%= model %>: this.<%= model %>ToUpdate,
+            onUpdate: this.onUpdate.bind(this),
+            cancel: this.cancel.bind(this)
+        };
+
+        this.modalToggle.emit(info);
+    }
+}
