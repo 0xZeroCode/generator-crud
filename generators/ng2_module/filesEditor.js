6da2f537793341f127b3b29a9dc0026f9c39f2db
irakli jiishkariani <irakli2692@gmail.com>
2017-07-14 09:35:59 +0400

    remove old edit functions

--- generators/ng2_module/filesEditor.js
+++ generators/ng2_module/filesEditor.js
@@ -51,35 +51,3 @@ exports.editAppModule = function (generator) {
 
   generator.fs.write(generator.destinationPath(appModuleFile), contents);
 };
-
-exports.insertModuleInImports = function (generator) {
-  const importsRegex = /imports:.*?\[((.|\n)+?)\]/im;
-
-  let contents = generator.fs.read(generator.destinationPath(appModuleFile));
-
-  let groups = importsRegex.exec(contents);
-
-  let toReplace = `${groups[1]},
-  ${names.toModuleName(generator.moduleName)}`;
-
-  let newImport = groups[0].replace(groups[1], toReplace);
-
-  contents = contents.replace(groups[0], newImport);
-
-  generator.fs.write(generator.destinationPath(appModuleFile), contents);
-};
-
-exports.addImportLine = function (generator) {
-  const name = generator.moduleName;
-  const componentImport = `import { AppComponent } from './app.component';`;
-
-  const toReplace = `import { ${names.toModuleName(name)} } from './${names.toFolderName(name)}/${names.toModuleFileName(name)}.module';
-
-  ${componentImport}`;
-
-  let contents = generator.fs.read(generator.destinationPath(appModuleFile));
-
-  contents = contents.replace(componentImport, toReplace);
-
-  generator.fs.write(generator.destinationPath(appModuleFile), contents);
-};
